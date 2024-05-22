3b741a251b71b405045410afca3b54bbf309ae2b
ottoheiskanen <ottoheiskanen@gmail.com>
2024-05-22 19:00:38 +0300

    add: svg rendering

--- app.rb
+++ app.rb
@@ -3,22 +3,19 @@ require 'sinatra'
 require './scraper.rb'
 require './generator.rb'
 
-
-
-# get '/' do
-#   scraper = Scraper.new("Wrath tiara")
-#   stats = scraper.stats
-#   generator = Generator.new()
-#   #generator.generate_svg_template(stats)
-#   puts stats
-# end
-
-
-get '/flex/' do
+get '/flex' do
   content_type 'image/svg+xml'
-  scraper = Scraper.new("Wrath tiara")
+
+  unless params[:rsn]
+    status 400
+    return 'error missing username params'
+  end
+
+  rsn = params[:rsn]
+
+  scraper = Scraper.new(rsn.to_s)
   stats = scraper.stats
+  puts stats
   generator = Generator.new()
-  svg = generator.svg
-  svg.render
+  generator.generate_stats_svg(stats, rsn)
 end
